8ae78dc277b6f95b2dca654744df4555eb684b7a
root <>
2021-12-31 10:40:56 +0530

    initial commit

---
+++ Printer.java
@@ -0,0 +1,13 @@
+
+public class Printer <T>{
+
+	T toPrint;
+	
+	public Printer(T toPrint) {
+		this.toPrint = toPrint;
+	}
+	
+	public void print() {
+		System.out.println(toPrint);
+	}
+}
---
+++ PrinterExec.java
@@ -0,0 +1,20 @@
+
+public class PrinterExec {
+
+	public static void main(String args[]) {
+		
+		/**
+		 * Avoid the need to create multiple classes for int long and string. Reduce boilerplate code.
+		 * Type safety for the collection objects
+		 */
+		Printer<Integer> intPrinter = new Printer<>(50);
+		intPrinter.print();
+		
+		Printer<Double> doublePrinter = new Printer<>(50.5d);
+		doublePrinter.print();
+		
+		Printer<String> stringPrinter = new Printer<>("This is String");
+		stringPrinter.print();
+		
+	}
+}
